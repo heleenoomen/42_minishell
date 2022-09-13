7ef97a873252df043d12493cc29fbd0ff8d5e7be
Kanykei Tashbaeva <76934648+smkatash@users.noreply.github.com>
2022-09-13 15:05:51 +0200

    Heredoc destruction helper functions added.

--- destructor/destroy.h
+++ destructor/destroy.h
@@ -23,6 +23,9 @@ void	free_ast_node(t_list **node);
 void	free_full_ast(t_ast **ast);
 void	free_cmd_defs(t_cmd_def **cmds);
 void	free_cmd_defs(t_cmd_def **cmds);
+void	heredoc_child_helper_destruction(t_exec *exec_cmds, t_minishell *shell);
+void	heredoc_helper_destruction(char **update, char **line, int *fd, t_exec *exec_cmds);
+void	heredoc_helper_destruction2(char *delim, int status);
 
 
 #endif
